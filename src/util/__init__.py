7b1c1020363370f8562f350d6233a5cfc465535a
John Murray <me@johnmurray.io>
2014-05-27 09:08:23 -0400

    AD-1485 - Extracted node + utils
    Extracted out SimpleNode into the 'node' module and ChainUtil
    and MetaDataUtil into the 'util' module.

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,5 +1,10 @@
-# Import like so to avoid stupid import in schema
-# from util import ChainUtil
-#  instead of
-# from util.chain_util import ChainUtil
-# from chain import *
+# Imports to avoid stupid import(s) in other files. This allows us to
+# put util classes in their own files without having to worry about
+# crappy imports. In the other files we want:
+#    from util import ChainUtil
+# instead of
+#    from util.chain_util import ChainUtil
+
+
+from chain import *
+from metadata import *
